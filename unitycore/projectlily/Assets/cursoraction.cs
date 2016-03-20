d28eef22e5e12df85603c8b57055d6f3dfd8573a
sirerr <sirerr@gmail.com>
2016-03-20 00:13:59 -0400

    getting back into this, just updating from what I've done

---
+++ unitycore/projectlily/Assets/cursoraction.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cursoraction : MonoBehaviour {
+
+	public Camera cameraobj;
+	private Vector3 originalscale;
+
+	// Use this for initialization
+	void Start () {
+		originalscale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		RaycastHit hit;
+		float distance;
+
+		if(Physics.Raycast (new Ray (cameraobj.transform.position,
+			cameraobj.transform.rotation * Vector3.forward),
+			out hit))
+			{
+				distance = hit.distance;
+			}
+		else
+		{
+			distance = cameraobj.farClipPlane * 0.99f;
+		}
+		transform.position = cameraobj.transform.position + cameraobj.transform.rotation *Vector3.forward * 
+			distance;
+		transform.LookAt(cameraobj.transform.position);
+		transform.Rotate(0,180f,0);
+		if(distance<10f)
+		{
+			distance *= 1+5*Mathf.Exp(-distance);
+		}
+		transform.localScale= originalscale * distance;
+	
+	}
+}
